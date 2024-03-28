1383f864d3398bb0be51d8388958a008fce7fa8a
Bíró Erik <biro.erik00@gmail.com>
2024-03-28 09:01:30 +0100

    cors

--- api/quiz.ts
+++ api/quiz.ts
@@ -46,5 +46,17 @@ const quiz: { question: string; answer: boolean }[] = [
 ];
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   return res.json(quiz);
 }
