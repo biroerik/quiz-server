5d9b3f91e4a73141bc856b78e96df0013ddcd925
Bíró Erik <biro.erik00@gmail.com>
2024-03-28 08:13:16 +0100

    Added questions

---
+++ api/quiz.ts
@@ -0,0 +1,50 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+const quiz: { question: string; answer: boolean }[] = [
+  {
+    question: "Bayern München is the best team in the world",
+    answer: true,
+  },
+  {
+    question: "Bayern has the most Champions League titles",
+    answer: false,
+  },
+  {
+    question: "Bayern has the most Bundesliga titles",
+    answer: true,
+  },
+  {
+    question: "Thomas Mülles has the most caps for Bayern",
+    answer: false,
+  },
+  {
+    question: "Robert Lewandowski scored the most goals for Bayern",
+    answer: false,
+  },
+  {
+    question: "Bayern was foundeded in 1900",
+    answer: true,
+  },
+  {
+    question:
+      "Bayern is the only club who won the Champions League 3 times in a row",
+    answer: false,
+  },
+  {
+    question: "Bayern achived the treble one time",
+    answer: false,
+  },
+  {
+    question: "Bayern home stadium's name is the Allianz Arena",
+    answer: true,
+  },
+  {
+    question:
+      "In the 2013 Champions League final the winning goal was scored by Arjen Robben",
+    answer: true,
+  },
+];
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return res.json(quiz);
+}
